23bbf5df35fe9996e1200cdaefe29c927ac99fd0
Muhamed Fall <fallmuhamed@gmail.com>
2018-12-16 23:52:29 -0500

    Added files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MessageList from '../src/components/MessageList'
+import SendMessageForm from '../src/components/SendMessageForm'
+import RoomList from '../src/components/RoomList'
+import NewRoomForm from '../src/components/NewRoomForm'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import { tokenUrl, instanceLocator } from './config'
+
+// import { tokenUrl, instanceLocator, userId, roomId } from './config'
+import { TokenProvider, ChatManager } from '@pusher/chatkit-client'
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            messages: []
+        }
+    }
+
+    componentDidMount() {
+        const chatKitManager = new ChatManager({
+            instanceLocator,
+            userId: 'devP',
+            tokenProvider: new TokenProvider({
+                url: tokenUrl
+            })
+        })
+
+        chatKitManager.connect()
+        .then(currentUser => {
+            currentUser.subscribeToRoom({
+                roomId: 19528602,
+                hooks: {
+                    onNewMessage: message => {
+                        this.setState({
+                            messages: [...this.state.messages, message]
+                        })                        
+                    }
+                }
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <RoomList />
+                <MessageList messages={this.state.messages} />
+                <SendMessageForm />
+                <NewRoomForm />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
---
+++ src/config.js
@@ -0,0 +1,15 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/b233b9b8-4fcc-4968-9683-4907a8cf705c/token"
+const instanceLocator = "v1:us1:b233b9b8-4fcc-4968-9683-4907a8cf705c"
+
+// const userId = "devP"
+
+// const roomId = 19528602
+
+// exports.userName = userName
+// exports.roomId = roomId
+// exports.tokenUrl = tokenUrl
+// exports.instanceLocator = instanceLocator
+
+// export { tokenUrl, instanceLocator, userId, roomId }
+
+export { tokenUrl, instanceLocator }
